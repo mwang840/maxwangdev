b408079a41d7b2856ab7bef9f4d2a59608fc906d
mwang840 <maxwang@udel.edu>
2022-12-15 09:27:15 -0500

    feat added another test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -12,3 +12,9 @@ test('There exists am image shown on the homepage', ()=>{
    const max = screen.getByRole('img');
    expect(max).toHaveAttribute('alt', 'Maxwell Wang');
 });
+
+test('There is a description of what I want to do', ()=>{
+  render(<App/>);
+  const fullStack = screen.getByText(/Aspiring Full-Stack Developer/i);
+  expect(fullStack).toBeInTheDocument();
+})
