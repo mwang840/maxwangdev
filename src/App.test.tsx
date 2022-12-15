1939adffb6038ee0f91fa9d81bf8a77e31e6a866
mwang840 <maxwang@udel.edu>
2022-12-14 21:01:04 -0500

    phase one of getting image up is done

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
+test('Shows name on website', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Maxwell Wang/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('There exists am image shown on the homepage', ()=>{
+   render(<App/>);
+   const max = screen.getByRole('img');
+   expect(max).toHaveAttribute('alt', 'Maxwell Wang');
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Maxwell from "./assets/MaxWang.jpg";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Maxwell} alt="Maxwell Wang" width="600" height="500" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Maxwell Wang
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
