190dae12898281e10b8ccdfa0a113257d7bb42c8
mwang840 <maxwang@udel.edu>
2023-01-02 20:55:09 -0500

    fixed linter errors according to the rules set up in the linting file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 //import Maxwell from "./assets/MaxWang.jpg";
-import { ViewNavbar } from './components/ViewNavbar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import { AboutMe } from './components/AboutMe';
-import { Languages } from './components/Languages';
+import { ViewNavbar } from "./components/ViewNavbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { AboutMe } from "./components/AboutMe";
+import { Languages } from "./components/Languages";
  
 function App() {
-  return (
-    <>
-    <h1>Maxwell Wang</h1>
-       <BrowserRouter>
-       <ViewNavbar/>
-        <Routes>
-            <Route path="/aboutme" element={<AboutMe/>}/>
-            <Route path="/languages" element={<Languages/>}/>
-        </Routes>
-        </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <h1>Maxwell Wang</h1>
+            <BrowserRouter>
+                <ViewNavbar/>
+                <Routes>
+                    <Route path="/aboutme" element={<AboutMe/>}/>
+                    <Route path="/languages" element={<Languages/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
