e2d47836b54b22290ffc55220ee0f632d4f27b51
mwang840 <maxwang@udel.edu>
2023-01-02 19:38:48 -0500

    one big fat commit

--- src/components/ViewNavbar.tsx
+++ src/components/ViewNavbar.tsx
@@ -1,49 +1,36 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import "./ViewNavbarModule.css";
+//import {Link} from "react-router-dom";
+import styles from "./ViewNavbarModule.css";
+import data from "./links.json";
+const linkToString = JSON.stringify(data);
+const links = JSON.parse(linkToString).links;
+
+type Link = {
+  label: string;
+  href: string;
+};
+
+const Links: React.FC<{links: Link[]}> = ({links})=>{
+    return(
+        <div className={styles["links-container"]}>
+            {links.map((link: Link)=>{
+                return(
+                    <div key={link.href} className={styles["link"]}>
+                        <a href={link.href}>
+                            {link.label}
+                        </a>
+                    </div>
+                );
+            })}
+        </div>);
+};
+
+
 // eslint-disable-next-line @typescript-eslint/ban-types
 export const ViewNavbar: React.FC<{}> = () => {
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to={'/'}>Home</Link>
-              </li>
-              <li className="nav-item"><Link className="nav-link" to={'/aboutme'}>About Me</Link></li>
-              <li className="nav-item">
-                <Link className="nav-link" to={'/languages'}>Languages</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={'/projects'}>
-                    Projects
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={'/contact'}>
-                   Contact
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Feedback</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+    return (
+        <nav className={styles.navbar}>
+            <Links links={links}/>
+        </nav> 
+    );
 };
