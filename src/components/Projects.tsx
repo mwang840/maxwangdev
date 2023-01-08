e671402e0ae5cf105d5b13e4b7581b2d674392bd
mwang840 <maxwang@udel.edu>
2023-01-08 09:25:26 -0500

    projects finished

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,9 @@ import scheduler from "./assets/cis-scheduler.png";
 import "./Projects.css";
 import { CISModal } from "./models/CISModal";
 import { RINMODAL } from "./models/RINModal";
+import { PIModal } from "./models/PIModal";
 import react from "./assets/reactinANutshell.png";
+import pi from "./assets/raspberrypi.jpg";
 
 export function Projects():JSX.Element{
     return (<div>
@@ -11,7 +13,7 @@ export function Projects():JSX.Element{
         <h3>Feel free to check out the projects that I have done whether it was in a group setting or not. Click on the links below each image to learn more!</h3>
         <img src={scheduler} alt="Course Scheduler" className="cis-center"></img> <CISModal/>
         <img src={react} alt="React" className="react-center"></img><RINMODAL/>
-        
+        <img src={pi} alt="Raspberry Pi" className="pi-center"></img><PIModal/>
         
     </div>);
 }
