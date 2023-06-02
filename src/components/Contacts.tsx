a893397b53d1180ad05b68522e214ce1bf8cee37
mwang840 <maxwang@udel.edu>
2023-06-02 15:32:46 -0400

    Added typewriter components under the svgs, not sure if it looks good or not

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,13 +1,71 @@
+/* eslint-disable indent */
 import React from "react";
 import "./Contacts.css";
 import { FaGithub, FaInstagram, FaLinkedin, FaMailBulk } from "react-icons/fa";
-
-export function Contacts(): JSX.Element{
-    return <div>
-        <h1>Contact Information</h1>
-        <a href="https://github.com/mwang840"><FaGithub size={70} color="black"/></a>
-        <a href="https://www.linkedin.com/in/maxwell-wang-08ws/"><FaLinkedin size={70} color="aqua"/></a> 
-        <a href="https://www.instagram.com/iammaxwellw/"><FaInstagram size={70} color="purple"/></a>
-        <a href="mailto:maxwell.y.wang@gmail.com, maxwang@udel.edu"><FaMailBulk size={71} color="gold"/></a>
-    </div>;
+import TypewriterComponent from "typewriter-effect";
+export function Contacts(): JSX.Element {
+  return (
+    <div className="contact">
+      <TypewriterComponent
+        onInit={(typewriter) => {
+          typewriter
+            .typeString("Contact Me")
+            .start()
+            .pauseFor(2000)
+            .deleteAll()
+            .typeString("If you have any questions!!");
+        }}
+      />
+      <a href="https://github.com/mwang840">
+        <FaGithub size={70} color="black" />
+        <TypewriterComponent
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("mwang840")
+              .start()
+              .pauseFor(2000)
+              .deleteAll();
+          }}
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/maxwell-wang-08ws/">
+        <FaLinkedin size={70} color="aqua" />
+        <TypewriterComponent
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("Maxwell Wang")
+              .start()
+              .pauseFor(2000)
+              .deleteAll();
+          }}
+        />
+      </a>
+      <a href="https://www.instagram.com/iammaxwellw/">
+        <FaInstagram size={70} color="purple" />
+        <TypewriterComponent
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("@iammaxwellw")
+              .start()
+              .pauseFor(2000)
+              .deleteAll();
+          }}
+        />
+      </a>
+      <a href="mailto:maxwell.y.wang@gmail.com, maxwang@udel.edu">
+        <FaMailBulk size={71} color="gold" />
+        <TypewriterComponent
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("maxwell.y.wang@gmail.com (personal)")
+              .start()
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString("maxwang@udel.edu (school)")
+              .deleteAll();
+          }}
+        />
+      </a>
+    </div>
+  );
 }
