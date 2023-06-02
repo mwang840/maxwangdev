3591cfc930d19b23b80571c27c1d6a136c67a8f7
mwang840 <maxwang@udel.edu>
2023-06-01 21:42:39 -0400

    Feat typewriter effect, had to disable linting : (

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import React from "react";
 import { ViewNavbar } from "./components/ViewNavbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -6,22 +7,32 @@ import { AboutMe } from "./components/AboutMe";
 import { Languages } from "./components/Languages";
 import { Projects } from "./components/Projects";
 import { Contacts } from "./components/Contacts";
- 
+import TypewriterComponent from "typewriter-effect";
 function App() {
-    return (
-        <>
-            <h1>Maxwell Wang</h1>
-            <BrowserRouter>
-                <ViewNavbar/>
-                <Routes>
-                    <Route path="/aboutme" element={<AboutMe/>}/>
-                    <Route path="/languages" element={<Languages/>}/>
-                    <Route path="/projects" element={<Projects/>}/>
-                    <Route path="/contact" element={<Contacts/>}/>
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <div className="App">
+      <TypewriterComponent
+        onInit={(typweriter) => {
+          typweriter
+            .typeString("Hello there 👋")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("I'm Maxwell Wang")
+            .start();
+        }}
+      />
+
+      <BrowserRouter>
+        <ViewNavbar />
+        <Routes>
+          <Route path="/aboutme" element={<AboutMe />} />
+          <Route path="/languages" element={<Languages />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contacts />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
