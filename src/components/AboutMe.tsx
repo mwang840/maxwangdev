840625dc067c5f723af738d552028e1ff8bcc6e3
mwang840 <maxwang@udel.edu>
2023-06-11 11:47:51 -0600

    transfer about me e.c into accordions

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,9 +2,95 @@
 import React from "react";
 import Maxwell from ".//assets/MaxWang.jpg";
 import "./AboutMe.css";
+import { Accordion } from "./Accordion";
 import TypewriterComponent from "typewriter-effect";
 
 export function AboutMe(): JSX.Element {
+  const activities = [
+    {
+      title: "Chess",
+      content: (
+        <div>
+          <p>
+            I have been insterested in and have started playing chess since
+            elementary school.I have participated in chess clubs throughout the
+            school year (grade school and college) and I am open to new chess
+            strategies. I also have a chess.com account if anyone likes to
+            follow me and play me in chess (see contact page)♔.
+          </p>
+        </div>
+      ),
+    },
+    {
+      title: "Exercising and Hiking",
+      content: (
+        <div>
+          <p>
+            At grade school, I was on the mens varsity soccer team and played
+            soccer as a forward. After soccer, I have grown into weightlifting
+            and started going to the gym to improve my strength to become
+            stronger everyday.
+          </p>
+          <p>
+            Currently during school and during this summer, I&apos;ve started to
+            improve my strength training by going to the gym to build up
+            strength and eventually muscle.
+          </p>
+          <p>
+            My first experience in hiking was when my family first traveled to
+            the Grand Canyon and hiked on the trails.
+          </p>
+
+          <p>
+            I eventually started to enjoy hiking later and occasionally go out
+            with friends or family to hike in different places!
+          </p>
+        </div>
+      ),
+    },
+    {
+      title: "Traveling",
+      content: (
+        <div>
+          <p>
+            A hobby that I enjoyed, I like to go see places around the world. As
+            a young child, my family will take me to Taiwan to visit family.
+            From that moment, I&apos;ve started appreicating where I go to
+            capture the memories I make when traveling.
+          </p>
+          <p>
+            Countries that I have been to are
+            <li>Taiwan</li>
+            <li>Japan</li>
+            <li>South Korea</li>
+            <li>Hong Kong</li>
+            <li>Canada</li>
+          </p>
+          <p>
+            I&apos;m hoping I can go visit Europe, Africa, Latin America and the
+            rest of the United States
+          </p>
+        </div>
+      ),
+    },
+    {
+      title: "Aviation",
+      content: (
+        <div>
+          <p>
+            Similar to traveling, but I&apos;ve enjoyed planes ever since my
+            parents took me to Taiwan to visit family. I like passanger planes
+            and my favorite is the Boeing 777. I&aposve yet to fly the airbus
+            A-380!
+          </p>
+          <p>
+            I hope to get more exposure to different airlines as I travel around
+            the world and get to have a good time on those airlines
+          </p>
+        </div>
+      ),
+    },
+  ];
   return (
     <div>
       <TypewriterComponent
@@ -18,63 +104,31 @@ export function AboutMe(): JSX.Element {
         }}
       />
       <img src={Maxwell} alt="Maxwell Wang" className="about-me-image"></img>
+      <h1>Greetings</h1>
       <p>
-        Greetings, my name is Maxwell Wang and I am a senior year Computer
-        Science Bachelors of Science student concentrating in Artificial
-        Intelligence at the University of Delaware.
+        My name is Maxwell Wang and I am a senior year Computer Science
+        Bachelors of Science student concentrating in Artificial Intelligence at
+        the University of Delaware.
       </p>
       <p>
         I am interested in focusing in software engineering, web development,
-        artificial intelligence and machine learning.
+        artificial intelligence and machine learning/data science.
       </p>
       <p>
-        Further, I have a passion for learning Computer Science and learning as
-        much as I can about the world around me with Computer Science
-        applications.
+        At the University of Delaware, I have already taken or will be taking a
+        lot of computer science courses, each one specializing in their own
+        field.
       </p>
       <p>
-        Outside of Computer Science here are some things outside of Computer
-        Science that I like to do:
+        The computer science courses include: Software Engineering, Algorithms,
+        Automata Theory, Data Mining, Database Systems, Logic for Programming,
+        Operating Systems, Educational Game Development, Advanced Web
+        Technologies, Computer Vision and Machine Learning
       </p>
-      <p>
-        Chess: At a very young age, I have picked up an interest in playing
-        chess to improve my problem-solving and strategy development skills.
-      </p>
-      <p>
-        I have participated in chess clubs throughout the school year (grade
-        school and college) and I am open to new chess strategies. I also have a
-        chess.com account if anyone likes to follow me and play me in chess (see
-        contact page)♔.
-      </p>
-      <p>
-        Exercising: At grade school, I was on the mens varsity soccer team and
-        played soccer as a forward. After soccer, I have grown into
-        weightlifting and started going to the gym to improve my strength to
-        become stronger everyday.
-      </p>
-      <p>
-        Hiking: My first experience in hiking was when my family first traveled
-        to the Grand Canyon and hiked on the trails.
-      </p>
-      <p>
-        {" "}
-        Even though, I did not like the hike as I was tired easily, I eventually
-        started to enjoy hiking later in Southern Utah visiting national parks
-        such as Arches, Zion, and others and enjoyed the nature and the scenery
-        there.
-      </p>
-      <p>
-        {" "}
-        Recently, I have been hiking locally even seeing the Lehigh Gorge up in
-        PA.
-      </p>
-      <p>
-        Aviation: When I started traveling to see family in Taiwan, I usually
-        travel by plane. From this, it opened up my knowledge of aviation,
-        knowing certain types of planes and looking at airports (artitecture,
-        size and runways).
-      </p>
-      <p> I am hoping I can travel more to explore my aviation side passion.</p>
+      <h1>
+        Click on the dropdown to see activities I do outside of computer science
+      </h1>
+      <Accordion items={activities}></Accordion>
     </div>
   );
 }
